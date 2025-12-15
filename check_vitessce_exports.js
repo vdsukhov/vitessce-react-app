84cc74c432495ad495db1c3bd8fc0c3ef9035617
Vladimir Sukhov <vdsukhov@ya.ru>
2025-12-15 13:53:50 -0600

    Example with using Vitessce JS API

---
+++ check_vitessce_exports.js
@@ -0,0 +1,6 @@
+import * as vitessce from 'vitessce';
+console.log(Object.keys(vitessce));
+console.log('CoordinationType:', !!vitessce.CoordinationType);
+console.log('coordinationType:', !!vitessce.coordinationType);
+console.log('VitessceConfig:', !!vitessce.VitessceConfig);
+console.log('hconcat:', !!vitessce.hconcat);
--- src/pages/DynamicPage.jsx
+++ src/pages/DynamicPage.jsx
@@ -1,268 +1,90 @@
 import { useParams } from 'react-router-dom';
-import { Vitessce } from 'vitessce';
+import { Vitessce, VitessceConfig, ViewType as vt, CoordinationType as ct, FileType as ft, hconcat, vconcat } from 'vitessce';
 import '../App.css';
 
 function DynamicPage() {
     const { id } = useParams();
 
-    const config = {
-        "version": "1.0.17",
-        "name": "Habib et al., 2017 Nature Methods",
-        "description": "Archived frozen adult human post-mortem brain tissue profiled by snRNA-seq (DroNc-seq)",
-        "datasets": [
-            {
-                "uid": "habib-2017",
-                "name": "Habib 2017",
-                "files": [
-                    {
-                        "fileType": "anndata.zarr",
-                        "url": "https://storage.googleapis.com/vitessce-demo-data/habib-2017/habib17.processed.h5ad.zarr",
-                        "coordinationValues": {
-                            "obsType": "cell",
-                            "featureType": "gene",
-                            "featureValueType": "expression",
-                            "embeddingType": "UMAP"
-                        },
-                        "options": {
-                            "obsFeatureMatrix": {
-                                "path": "X",
-                                "initialFeatureFilterPath": "var/top_highly_variable"
-                            },
-                            "obsEmbedding": {
-                                "path": "obsm/X_umap"
-                            },
-                            "obsSets": [
-                                {
-                                    "name": "Cell Type",
-                                    "path": "obs/CellType"
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        ],
-        "initStrategy": "auto",
-        "coordinationSpace": {
-            "dataset": {
-                "A": "habib-2017"
-            },
-            "embeddingType": {
-                "A": "UMAP"
-            },
-            "embeddingZoom": {
-                "A": 3
-            },
-            "embeddingTargetX": {
-                "A": 0
-            },
-            "embeddingTargetY": {
-                "A": 0
-            },
-            "embeddingTargetZ": {
-                "A": 0
-            },
-            "embeddingObsSetPolygonsVisible": {
-                "A": false
-            },
-            "embeddingObsSetLabelsVisible": {
-                "A": false
-            },
-            "embeddingObsSetLabelSize": {
-                "A": 14
-            },
-            "embeddingObsRadius": {
-                "A": 1
-            },
-            "embeddingObsRadiusMode": {
-                "A": "auto"
-            },
-            "embeddingObsOpacity": {
-                "A": 1
-            },
-            "embeddingObsOpacityMode": {
-                "A": "auto"
-            },
-            "obsType": {
-                "A": "cell"
-            },
-            "featureType": {
-                "A": "gene"
-            },
-            "featureValueType": {
-                "A": "expression"
-            },
-            "obsColorEncoding": {
-                "A": "cellSetSelection"
-            },
-            "featureSelection": {
-                "A": null
-            },
-            "featureValueColormap": {
-                "A": "viridis"
-            },
-            "featureValueColormapRange": {
-                "A": [
-                    0,
-                    1
-                ]
-            },
-            "obsSetSelection": {
-                "A": [
-                    [
-                        "Cell Type",
-                        "Excitatory"
-                    ]
-                ]
-            },
-            "obsSetColor": {
-                "A": null
-            },
-            "additionalObsSets": {
-                "A": null
-            },
-            "heatmapZoomX": {
-                "A": 0
-            },
-            "heatmapZoomY": {
-                "A": 0
-            },
-            "heatmapTargetX": {
-                "A": 0
-            },
-            "heatmapTargetY": {
-                "A": 0
-            },
-            "obsFilter": {
-                "A": null
-            },
-            "featureFilter": {
-                "A": null
-            },
-            "obsHighlight": {
-                "A": null
-            },
-            "featureHighlight": {
-                "A": null
-            }
-        },
-        "layout": [
-            {
-                "component": "obsSets",
-                "h": 4,
-                "w": 4,
-                "x": 0,
-                "y": 0,
-                "coordinationScopes": {
-                    "dataset": "A",
-                    "obsType": "A",
-                    "obsSetSelection": "A",
-                    "obsSetColor": "A",
-                    "obsColorEncoding": "A",
-                    "additionalObsSets": "A",
-                    "obsFilter": "A",
-                    "obsHighlight": "A",
-                    "featureSelection": "A",
-                    "featureValueColormap": "A",
-                    "featureValueColormapRange": "A"
-                }
-            },
-            {
-                "component": "scatterplot",
-                "h": 4,
-                "w": 4,
-                "x": 4,
-                "y": 0,
-                "coordinationScopes": {
-                    "dataset": "A",
-                    "obsType": "A",
-                    "featureType": "A",
-                    "featureValueType": "A",
-                    "embeddingType": "A",
-                    "embeddingZoom": "A",
-                    "embeddingTargetX": "A",
-                    "embeddingTargetY": "A",
-                    "embeddingTargetZ": "A",
-                    "embeddingObsSetPolygonsVisible": "A",
-                    "embeddingObsSetLabelsVisible": "A",
-                    "embeddingObsSetLabelSize": "A",
-                    "embeddingObsRadius": "A",
-                    "embeddingObsRadiusMode": "A",
-                    "embeddingObsOpacity": "A",
-                    "embeddingObsOpacityMode": "A",
-                    "obsColorEncoding": "A",
-                    "featureSelection": "A",
-                    "featureValueColormap": "A",
-                    "featureValueColormapRange": "A",
-                    "obsSetSelection": "A",
-                    "obsSetColor": "A",
-                    "additionalObsSets": "A",
-                    "obsFilter": "A",
-                    "obsHighlight": "A",
-                    "featureHighlight": "A"
-                }
-            },
-            {
-                "component": "heatmap",
-                "h": 4,
-                "w": 8,
-                "x": 0,
-                "y": 4,
-                "coordinationScopes": {
-                    "dataset": "A",
-                    "obsType": "A",
-                    "featureType": "A",
-                    "featureValueType": "A",
-                    "obsColorEncoding": "A",
-                    "featureSelection": "A",
-                    "featureValueColormap": "A",
-                    "featureValueColormapRange": "A",
-                    "obsSetSelection": "A",
-                    "obsSetColor": "A",
-                    "additionalObsSets": "A",
-                    "heatmapZoomX": "A",
-                    "heatmapZoomY": "A",
-                    "heatmapTargetX": "A",
-                    "heatmapTargetY": "A",
-                    "obsFilter": "A",
-                    "featureFilter": "A",
-                    "obsHighlight": "A",
-                    "featureHighlight": "A"
-                }
-            },
-            {
-                "component": "featureList",
-                "h": 4,
-                "w": 4,
-                "x": 8,
-                "y": 0,
-                "coordinationScopes": {
-                    "dataset": "A",
-                    "obsType": "A",
-                    "featureType": "A",
-                    "featureValueType": "A",
-                    "featureSelection": "A",
-                    "obsColorEncoding": "A",
-                    "featureFilter": "A",
-                    "obsHighlight": "A",
-                    "featureHighlight": "A"
-                }
-            }
-        ]
-    };
+    try {
+        const vc = new VitessceConfig({
+            schemaVersion: "1.0.17",
+            name: "Habib et al., 2017 Nature Methods",
+            description: "Archived frozen adult human post-mortem brain tissue profiled by snRNA-seq (DroNc-seq)",
+        });
 
-    return (
-        <div className="container">
-            <h1 className="dynamic-title">{id}</h1>
-            <div className="vitessce-container">
-                <Vitessce
-                    height={600}
-                    theme="dark"
-                    config={config}
-                />
+        const dataset = vc.addDataset("Habib 2017")
+            .addFile({
+                url: "https://storage.googleapis.com/vitessce-demo-data/habib-2017/habib17.processed.h5ad.zarr",
+                fileType: ft.ANNDATA_ZARR,
+                coordinationValues: {
+                    obsType: "cell",
+                    featureType: "gene",
+                    featureValueType: "expression",
+                    embeddingType: "UMAP"
+                },
+                options: {
+                    obsFeatureMatrix: {
+                        path: "X",
+                        initialFeatureFilterPath: "var/top_highly_variable"
+                    },
+                    obsEmbedding: {
+                        path: "obsm/X_umap",
+                        dims: [0, 1],
+                        embeddingType: "UMAP"
+                    },
+                    obsSets: [
+                        {
+                            name: "Cell Type",
+                            path: "obs/CellType"
+                        }
+                    ]
+                }
+            });
+
+        const obsSets = vc.addView(dataset, vt.OBS_SETS);
+        const scatterplot = vc.addView(dataset, vt.SCATTERPLOT, { mapping: "UMAP" });
+        const heatmap = vc.addView(dataset, vt.HEATMAP);
+        const featureList = vc.addView(dataset, vt.FEATURE_LIST);
+
+        vc.layout(
+            hconcat(
+                vconcat(obsSets, heatmap),
+                vconcat(scatterplot, featureList)
+            )
+        );
+
+        // Set initial coordination values
+        const [embeddingZoomScope] = vc.addCoordination(ct.EMBEDDING_ZOOM);
+        embeddingZoomScope.setValue(3);
+        scatterplot.useCoordination(embeddingZoomScope);
+
+        const [obsSetSelectionScope] = vc.addCoordination(ct.OBS_SET_SELECTION);
+        obsSetSelectionScope.setValue([["Cell Type", "Excitatory"]]);
+
+        obsSets.useCoordination(obsSetSelectionScope);
+        scatterplot.useCoordination(obsSetSelectionScope);
+        heatmap.useCoordination(obsSetSelectionScope);
+        featureList.useCoordination(obsSetSelectionScope);
+
+        const config = vc.toJSON();
+        console.log('Config generated successfully', config);
+
+        return (
+            <div className="container">
+                <h1 className="dynamic-title">{id}</h1>
+                <div className="vitessce-container">
+                    <Vitessce
+                        height={800}
+                        theme="dark"
+                        config={config}
+                    />
+                </div>
             </div>
-        </div>
-    );
+        );
+    } catch (e) {
+        console.error('Error generating config:', e);
+        return <div className="error">Error generating config: {e.message}</div>;
+    }
 }
 
 export default DynamicPage;
